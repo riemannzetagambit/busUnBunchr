9db6c926bea69547db267270a62b688c0c8ca345
David Stone <david.curtis.stone@roma1.infn.it>
2016-02-04 15:54:37 -0800

    Massive reorganization, only tracking busUnBunchr_site, with all files there

--- compute_distance_distributions.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-
-# in this file read in data from the specified database and rebuild the distance distributions
-# writing them as numpy arrays to the specified directory
---
+++ run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from busUnBunchr_site import app
+
+app.run(debug = True)

1f38398e2e7d65eaa9e00d77c0190328f554e66d
David Stone <david.curtis.stone@roma1.infn.it>
2016-02-10 23:11:01 -0800

    Made crucial changes to get_frequency_for_route, changes that were made before but got lost in the git soup

--- busUnBunchr_site/get_frequency_for_route.py
+++ busUnBunchr_site/get_frequency_for_route.py
@@ -10,8 +10,9 @@ def get_frequency_for_route(pair_route, pair_time):
 	''' Must past pair_time as a time stamp 
 		(which should come from dataframe that you are adding to) 
 	'''
-	with open('route_frequencies/route_'+str(pair_route)+'_frequencies.pkl','rb') as input:
+	with open('busUnBunchr_site/route_frequencies/route_'+str(pair_route)+'_frequencies.pkl','rb') as input:
 		freq_df  = pickle.load(input)
 	t0 = '1970-01-01 '+str(pair_time.hour)+':'+str(pair_time.minute)+':00'
-	t1 = '1970-01-01 '+str(pair_time.hour+2)+':'+str(pair_time.minute)+':00'
+	# set to end of day, no messing around
+	t1 = '1970-01-01 23:59:00'
 	return freq_df.loc[t0:t1]['freq'][0]	
